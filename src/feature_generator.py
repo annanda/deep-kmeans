c908648646db7820d7cb03f9c311f53715445470
Annanda Sousa <annanda.sousa@gmail.com>
2016-12-14 13:13:43 -0200

    Arrumando o pydoc

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -3,53 +3,49 @@ from sklearn.metrics import silhouette_score
 
 from image_utils import *
 
-
-
-# description: generate a set of centroids from image
-# input:
-#		data_set: a numpy array of images (numpy array)
-#		window_size: a tuple (width, height) with the sample size
-#		num_cluster: the number of centroids that will be generated
-# output:
-#		centroids: numpy array with the k-means generated centroids
 def generate_cluster_centroids(data_set, window_size, num_cluster):
-	
-	X = np.array(data_set[0])
-	y = data_set[1]
+    """generate a set of centroids from image
 
-	sqrt_num_cluster = int(np.sqrt(num_cluster))
+    :param data_set: a numpy array of images (numpy array)
+    :param window_size: a tuple (width, height) with the sample size
+    :param num_cluster: the number of centroids that will be generated
+    :return: centroids: numpy array with the k-means generated centroids
+    """
 
-	#  GENERATING SAMPLES
-	samples = generate_samples(X, 10000, window_size=(window_size,window_size))
+    X = np.array(data_set[0])
+    y = data_set[1]
 
-	# FITTING K-MEANS
-	kmeans_model =  MiniBatchKMeans(n_clusters=num_cluster, batch_size=100, n_init=10, max_no_improvement=10, verbose=False)
-	kmeans_model.fit(samples)
-	
-	centroids = kmeans_model.cluster_centers_
+    sqrt_num_cluster = int(np.sqrt(num_cluster))
 
-	return centroids
+    #  GENERATING SAMPLES
+    samples = generate_samples(X, 10000, window_size=(window_size,window_size))
 
+    # FITTING K-MEANS
+    kmeans_model =  MiniBatchKMeans(n_clusters=num_cluster, batch_size=100, n_init=10, max_no_improvement=10, verbose=False)
+    kmeans_model.fit(samples)
+
+    centroids = kmeans_model.cluster_centers_
+
+    return centroids
 
 
 def run():
 
-	train_set, valid_set, test_set = read_images_from_mnist("../mnist/mnist.pkl.gz")
+    train_set, valid_set, test_set = read_images_from_mnist("../mnist/mnist.pkl.gz")
 
-	window_centroids_size = 5
-	num_cluster = 16
+    window_centroids_size = 5
+    num_cluster = 16
 
-	# creating centroids from mnist
-	centroids = generate_cluster_centroids(train_set, window_centroids_size, num_cluster)
+    # creating centroids from mnist
+    centroids = generate_cluster_centroids(train_set, window_centroids_size, num_cluster)
 
-	# drawing the grid with centroids
-	draw_multiple_images(centroids,4,4)
+    # drawing the grid with centroids
+    draw_multiple_images(centroids,4,4)
 
-	# filtering and drawing a image
-	filtered = convolve_image(train_set[0][0].reshape(28,28), centroids[0].reshape(5,5))
-	draw_img(filtered)
-
+    # filtering and drawing a image
+    filtered = convolve_image(train_set[0][0].reshape(28,28), centroids[0].reshape(5,5))
+    draw_img(filtered)
 
 
 if __name__ == '__main__':
-	run()
+    run()
--- src/image_utils.py
+++ src/image_utils.py
@@ -5,141 +5,150 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# description: read image from mnist folder
-# input:
-#		file_path: path for mnist cpickled file
-# output:
-#		train_set (50k examples): [ [list of images], [list of labels related with each image]  ] 
-#		valid_set (10k examples): [ [list of images], [list of labels related with each image]  ] 
-#		test_set (10k examples): [ [list of images], [list of labels related with each image]  ] 
-#
-# observation:	image are normalized and whitenning following paper's 
-# 				instructions (http://www-cs.stanford.edu/~acoates/papers/coatesng_nntot2012.pdf)
-#
 def read_images_from_mnist(file_path, normalize=True, whitenning=True):
-	
-	f = gzip.open(file_path, 'rb')
-	train_set, valid_set, test_set = cPickle.load(f)
-	f.close()
+    """read image from mnist folder
 
-	for i in xrange(len(train_set[0])):
-		img = np.array(train_set[0][i]).reshape(28,28)
+    :param file_path: path for mnist cpickled file
+    :param normalize:
+    :param whitenning:
+    :return: train_set (50k examples): [ [list of images], [list of labels related with each image]  ]
+    :return: valid_set (10k examples): [ [list of images], [list of labels related with each image]  ]
+    :return: test_set (10k examples): [ [list of images], [list of labels related with each image]  ]
 
-		if normalize:
-			img = normalize_img(img)
+    note: image are normalized and whitenning following paper's
+            instructions (http://www-cs.stanford.edu/~acoates/papers/coatesng_nntot2012.pdf)
+    """
 
-		if whitenning:			
-			img = whitenning_img(img)
+    f = gzip.open(file_path, 'rb')
+    train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
 
-		train_set[0][i] = img.reshape(784)
+    for i in range(len(train_set[0])):
+        img = np.array(train_set[0][i]).reshape(28,28)
 
-	for i in xrange(len(valid_set[0])):
-		img = np.array(valid_set[0][i]).reshape(28,28)
+        if normalize:
+            img = normalize_img(img)
 
-		if normalize:
-			img = normalize_img(img)
+        if whitenning:
+            img = whitenning_img(img)
 
-		if whitenning:
-			img = whitenning_img(img)
+        train_set[0][i] = img.reshape(784)
 
-		valid_set[0][i] = img.reshape(784)
+    for i in range(len(valid_set[0])):
+        img = np.array(valid_set[0][i]).reshape(28,28)
 
-	for i in xrange(len(test_set[0])):
-		img = np.array(test_set[0][i]).reshape(28,28)
+        if normalize:
+            img = normalize_img(img)
 
-		if normalize:
-			img = normalize_img(img)
+        if whitenning:
+            img = whitenning_img(img)
 
-		if whitenning:
-			img = whitenning_img(img)
+        valid_set[0][i] = img.reshape(784)
 
-		test_set[0][i] = img.reshape(784)
+    for i in range(len(test_set[0])):
+        img = np.array(test_set[0][i]).reshape(28,28)
 
-	return train_set, valid_set, test_set
+        if normalize:
+            img = normalize_img(img)
+
+        if whitenning:
+            img = whitenning_img(img)
+
+        test_set[0][i] = img.reshape(784)
+
+    return train_set, valid_set, test_set
 
 
-# description: normalize values inside an image matrix 
-# input:
-#		img: numpy array
-# output:
-#		img: numpy array 
 def normalize_img(img):
-	return cv2.normalize(img, img, alpha=0.0, beta=1.0, norm_type=cv2.NORM_MINMAX)
+    """normalize values inside an image matrix
 
-# description: remove content (filter) image using a sobel filter
-# input:
-#		img: numpy array
-# output:
-#		img: numpy array 
+    :param img: numpy array
+    :return: img: numpy array
+    """
+    return cv2.normalize(img, img, alpha=0.0, beta=1.0, norm_type=cv2.NORM_MINMAX)
+
+
 def whitenning_img(img):
-	return cv2.Sobel(img,cv2.CV_32F,1,1,ksize=5)
+    """remove content (filter) image using a sobel filter
 
-# description: get a sample slice from a image
-# input:
-#		img: numpy array
-#		window_size: a tuple (width, height) with the sample size
-# output:
-#		img: numpy array 
+    :param img: numpy array
+    :return: numpy array
+    """
+    return cv2.Sobel(img,cv2.CV_32F,1,1,ksize=5)
+
+
 def sampling_image(img, window_size=(5,5)):
-	x = np.random.randint(0, img.shape[0]-window_size[0])
-	y = np.random.randint(0, img.shape[1]-window_size[1])
-	return img[x : x + window_size[0], y: y + window_size[1]].flatten()
+    """get a sample slice from a image
 
-# description: from a set of images, generate many samples (slices)
-# input:
-#		dataset: numpy array with multiple images (each image is a numpy array)
-#		num_samples: number of samples that must be generated
-#		window_size: a tuple (width, height) with the sample size
-# output:
-#		img: numpy array with multiple image samples
+    :param img: numpy array
+    :param window_size: a tuple (width, height) with the sample size
+    :return: img: numpy array
+    """
+    x = np.random.randint(0, img.shape[0]-window_size[0])
+    y = np.random.randint(0, img.shape[1]-window_size[1])
+    return img[x : x + window_size[0], y: y + window_size[1]].flatten()
+
+
 def generate_samples(data_set, num_samples, window_size=(5,5)):
-	samples = []
-	for i in range(num_samples):
-		random_img_posi = np.random.randint(0, len(data_set))
-		img = data_set[random_img_posi]
-		sample = sampling_image(img.reshape(28,28), window_size=window_size )
-		samples.append(sample)
-	return np.array(samples)
+    """from a set of images, generate many samples (slices)
 
-# description: draw multiple images (usually k-means centroids) in a grid
-# input:
-#		images: numpy array with multiple images (each image is a numpy array)
-#		num_lines: number of lines in grid
-#		num_columns: number of columns in grid
+    :param data_set: numpy array with multiple images (each image is a numpy array)
+    :param num_samples: number of samples that must be generated
+    :param window_size: tuple (width, height) with the sample size
+    :return: img: numpy array with multiple image samples
+    """
+    samples = []
+    for i in range(num_samples):
+        random_img_posi = np.random.randint(0, len(data_set))
+        img = data_set[random_img_posi]
+        sample = sampling_image(img.reshape(28,28), window_size=window_size )
+        samples.append(sample)
+    return np.array(samples)
+
+
 def draw_multiple_images(images, num_lines, num_columns):
+    """draw multiple images (usually k-means centroids) in a grid
 
-	img_is_flatten = True if len(images[0].shape) == 1 else False
+    :param images: numpy array with multiple images (each image is a numpy array)
+    :param num_lines: number of lines in grid
+    :param num_columns: number of columns in grid
+    """
 
-	fig, subs = plt.subplots(num_lines, num_columns)
-	for i in xrange(num_lines):
-		for j in xrange(num_columns):
-			img = images[i*j]
+    img_is_flatten = True if len(images[0].shape) == 1 else False
 
-			if img_is_flatten:
+    fig, subs = plt.subplots(num_lines, num_columns)
+    for i in range(num_lines):
+        for j in range(num_columns):
+            img = images[i*j]
 
-				width = np.sqrt(img.shape[0])
-				height = np.sqrt(img.shape[0])
-				subs[i][j].imshow(img.reshape(width, height),  cmap='Greys_r')
+            if img_is_flatten:
 
-			else:
-				subs[i][j].imshow(img,  cmap='Greys_r')
+                width = np.sqrt(img.shape[0])
+                height = np.sqrt(img.shape[0])
+                subs[i][j].imshow(img.reshape(width, height),  cmap='Greys_r')
 
-			subs[i][j].axis('off')
+            else:
+                subs[i][j].imshow(img,  cmap='Greys_r')
 
-	plt.show()
+            subs[i][j].axis('off')
 
-# description: draw an image
-# input:
-#		image: numpy array 
+    plt.show()
+
+
 def draw_img(img):
-	plt.imshow(img, cmap='Greys_r')
-	plt.show()
+    """ draw an image
 
-# description: apply a filter over a imge
-# input:
-#		img: numpy array representing original image
-#		filter: numpy array representing the filter that will be apply
-# output:
-#		img: numpy array with a filtered image
+    :param img: numpy array
+    """
+    plt.imshow(img, cmap='Greys_r')
+    plt.show()
+
+
 def convolve_image(img, filter):
-	return cv2.filter2D(img, -1, filter)
+    """apply a filter over a image
+
+    :param img: numpy array representing original image
+    :param filter: numpy array representing the filter that will be apply
+    :return: img: numpy array with a filtered image
+    """
+    return cv2.filter2D(img, -1, filter)
