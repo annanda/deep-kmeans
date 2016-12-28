6e4f8797fe6377428312875778572d95c388b645
Annanda Sousa <annanda.sousa@gmail.com>
2016-12-28 01:47:15 -0200

    Arrumando a função que pega os exemplos das classes

--- src/previa.py
+++ src/previa.py
@@ -33,55 +33,38 @@ def dic_to_array(dic):
     return array
 
 
-def read_images_from_cifar_10(file_path, normalize=True, whitenning=True):
+def read_images_from_cifar_10(data_files, test_files, normalize=True, whitenning=True):
     """
     Read images from cifar 10 folder and prepare the train set, valid set and test set
 
     :param file_path: path for cifar 10 cpickled file
     :param normalize:
     :param whitenning:
-    :return: train_set (4k examples): numpy ndarray shape(8000, 2). row[i][0] = [image], row[i][1] = label
-    :return: valid_set (1k examples): numpy ndarray shape(2000, 2). row[i][0] = [image], row[i][1] = label
-    :return: test_set (1k examples): numpy ndarray shape(2000, 2). row[i][0] = [image], row[i][1] = label
+    :return: train_set (8k examples): numpy ndarray shape(8000, 2). row[i][0] = [image], row[i][1] = label
+    :return: valid_set (2k examples): numpy ndarray shape(2000, 2). row[i][0] = [image], row[i][1] = label
+    :return: test_set (2k examples): numpy ndarray shape(2000, 2). row[i][0] = [image], row[i][1] = label
     """
 
-    train_dict = unpickle(file_path + 'data_batch_1')
+    cat_class = get_some_class(data_files, 'cat')
+    train_set = cat_class[:4000]
+    valid_set = cat_class[4000:]
 
-    test_dict = unpickle(file_path + 'test_batch')
+    bird_class = get_some_class(data_files, 'bird')
+    train_set.extend(bird_class[:4000])
+    valid_set.extend((bird_class[4000:]))
 
-    classe_1_train = train_dict['data'][:4000]
-    classe_1_valid = train_dict['data'][4000:5000]
-    classe_2_train = train_dict['data'][5000:9000]
-    classe_2_valid = train_dict['data'][9000:]
+    cat_test = get_some_class(test_files, 'cat')
+    test_set = cat_test
 
-    classe_1_train_label = train_dict['labels'][:4000]
-    classe_1_valid_label = train_dict['labels'][4000:5000]
-    classe_2_train_label = train_dict['labels'][5000:9000]
-    classe_2_valid_label = train_dict['labels'][9000:]
-
-    train = np.append(classe_1_train, classe_2_train, axis=0)
-    train_label = np.append(classe_1_train_label, classe_2_train_label, axis=0)
-    # make association between each image with its label
-    train_and_label = zip(train, train_label)
-    train_set = np.array(list(train_and_label))
-
-    valid_set = np.append(classe_1_valid, classe_2_valid, axis=0)
-    valid_label = np.append(classe_1_valid_label, classe_2_valid_label, axis=0)
-    # make association between each image with its label
-    valid_set = zip(valid_set, valid_label)
-    valid_set = np.array(list(valid_set))
-
-    test_dict['data'] = test_dict['data'][2000:4000]
-    test_dict['labels'] = test_dict['labels'][2000:4000]
-    test_set = dic_to_array(test_dict)
+    bird_test = get_some_class(test_files, 'bird')
+    test_set.extend(bird_test)
 
     return train_set, valid_set, test_set
 
 
-def get_class_1(class_name):
-    files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+def get_some_class(list_files, class_name):
     final_array = []
-    for file in files:
+    for file in list_files:
         dict = unpickle('../cifar-10/' + file)
         example_list = dic_to_array(dict)
         for example in example_list:
@@ -89,55 +72,6 @@ def get_class_1(class_name):
                 final_array.append(example)
     return final_array
 
-
-def get_classes():
-    files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
-    class_0 = []
-    class_1 = []
-    class_2 = []
-    class_3 = []
-    class_4 = []
-    class_5 = []
-    class_6 = []
-    class_7 = []
-    class_8 = []
-    class_9 = []
-    for file in files:
-        dict = unpickle('../cifar-10/' + file)
-        example_list = dic_to_array(dict)
-        for example in example_list:
-            class_number = example[1]
-            if class_number == 0:
-                class_0.append(example)
-            elif class_number == 1:
-                class_1.append(example)
-            elif class_number == 2:
-                class_2.append(example)
-            elif class_number == 3:
-                class_3.append(example)
-            elif class_number == 4:
-                class_4.append(example)
-            elif class_number == 5:
-                class_5.append(example)
-            elif class_number == 6:
-                class_6.append(example)
-            elif class_number == 7:
-                class_7.append(example)
-            elif class_number == 8:
-                class_8.append(example)
-            elif class_number == 9:
-                class_9.append(example)
-
-    print len(class_0)
-    print len(class_1)
-    print len(class_2)
-    print len(class_3)
-    print len(class_4)
-    print len(class_5)
-    print len(class_6)
-    print len(class_7)
-    print len(class_8)
-    print len(class_9)
 
 def draw_multiple_images(images, num_lines, num_columns):
     """
@@ -219,7 +153,12 @@ def run():
     # img_1 = examples_cats[0][0]
     # img = unflatten(img_1)
     # draw_img(img)
-    get_classes()
+    data_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+    test_files = ['test_batch']
+    train_set, valid_set, test_set = read_images_from_cifar_10(data_files, test_files)
+    print len(train_set)
+    print len(valid_set)
+    print len(test_set)
 
 if __name__ == '__main__':
     run()
