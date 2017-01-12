8b508fdc1e5ce4c8cea097b5ee19410e25f20b68
Annanda Sousa <annanda.sousa@gmail.com>
2017-01-12 04:46:41 -0200

    Colocando as funcoes de whitening nesse arquivo.

--- src/previa.py
+++ src/previa.py
@@ -4,6 +4,7 @@ import time
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import linalg
 
 LABEL_AIRPLANE = 0
 LABEL_AUTOMOBILE = 1
@@ -156,15 +157,14 @@ def draw_multiple_images(images, num_lines, num_columns):
     plt.show()
 
 
-def draw_img(img):
+def draw_img(img, title):
     """
     Draw an image
 
     :param img: numpy array
     """
-
-    plt.imshow(img)
-    plt.show()
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
 
 
 def unflatten(img_flatten):
@@ -200,53 +200,140 @@ def flatten_img(img):
     return np.array(img)
 
 
-def zca_whitening_matrix(X):
-    """
-    Function to compute ZCA whitening matrix (aka Mahalanobis whitening).
-    INPUT:  X: [M x N] matrix.
-        Rows: Variables
-        Columns: Observations
-    OUTPUT: ZCAMatrix: [M x M] matrix
-    """
-    # Covariance matrix [column-wise variables]: Sigma = (X-mu)' * (X-mu) / N
-    sigma = np.cov(X, rowvar=True)  # [M x M]
-    # Singular Value Decomposition. X = U * np.diag(S) * V
-    U, S, V = np.linalg.svd(sigma)
-    # U: [M x M] eigenvectors of sigma.
-    # S: [M x 1] eigenvalues of sigma.
-    # V: [M x M] transpose of U
-    # Whitening constant: prevents division by zero
-    epsilon = 1e-5
-    # ZCA Whitening matrix: U * Lambda * U'
-    ZCAMatrix = np.dot(U, np.dot(np.diag(1.0 / np.sqrt(S + epsilon)), U.T))  # [M x M]
-    return ZCAMatrix
-
-
-def apply_zca_in_image(img):
+def apply_whitening_in_image(img, is_zca=True):
     """
     Apply whitenning ZCA in an image
     :param img: nparray that is the image with three channels
+    :param is_zca: bool value that indicates if whitening is ZCA. If value is False,
+    whitening is PCA type.
     :return: nparray that is the image with three channels and whitened
     """
     b, g, r = cv2.split(img)
-    b = zca_whitening_matrix(b)
-    g = zca_whitening_matrix(g)
-    r = zca_whitening_matrix(r)
+    b = whiten_channel(b, is_zca)
+    g = whiten_channel(g, is_zca)
+    r = whiten_channel(r, is_zca)
 
     img = cv2.merge((b, g, r))
     return img
 
 
+'''Funcao responsavel por receber um array de imagens, uma opcao para apresentar imagens e dar resize nas mesmas.
+Para cada imagem do array sera calculada a sua matriz relativa a imagem esbranquicada e adicionada ao array de retorno.'''
+
+
+
+'''Funcao responsavel por receber uma imagem e retornar a matriz equivalente a mesma, porem esbranquicada.'''
+
+
+def whiten_channel(channel, is_zca=True):
+    width, height = channel.shape
+    shaped_channel = reshapeImage(channel)
+
+    xPCAWhite, U = PCAWhitening(shaped_channel)
+    xZCAWhite = ZCAWhitening(xPCAWhite, U)
+
+    if is_zca:
+        return shapeImageWhitened(xZCAWhite, width, height)
+
+    else:
+        return shapeImageWhitened(xPCAWhite, width, height)
+
+
+'''Funcao responsavel por ler a imagem original em tons de cinza, dar resize na mesma e apresenta-la
+(se as opcoes tiverem sido escolhidas). Retorna uma matriz 2D (pois transformamos a imagem para tons
+de cinza) NxM, onde N e M sao as dimensoes da imagem apos ter sido redimensionada (ou nao).'''
+
+
+# def loadData(img):
+#     img_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+#     if resizeImages:
+#         img_gray = cv2.resize(img_gray, (67, 540))
+#
+#     if showImages:
+#         showImage(img_gray, 'original')
+#     return img_gray
+
+
+'''Funcao responsavel por receber a matriz no formato 1 x N*M de valores float e realizar os passos do
+algoritmo de branqueamento do PCA. Sao eles: calcular sigma e seus autovalores (matriz U de rotacao, etc),
+achar xRot (dados rotacionados), xHat(dados em dimensao reduzida 1) e, finalmente, computar a matriz PCA
+utilizando a formula estabelecida. Retorna a matriz PCA e U.'''
+
+
+def PCAWhitening(x):
+    sigma = x.dot(x.T) / x.shape[1]
+    U, S, Vh = linalg.svd(sigma)
+
+    xRot = U.T.dot(x)
+
+    # Reduz o numero de dimensoes de 2 pra 1
+    k = 1
+    xRot = U[:, 0:k].T.dot(x)
+    xHat = U[:, 0:k].dot(xRot)
+
+    epsilon = 1e-5
+    xPCAWhite = np.diag(1.0 / np.sqrt(S + epsilon)).dot(U.T).dot(x)  # formula do PCAWhitening
+    return xPCAWhite, U
+
+
+'''Funcao responsavel por retornar a matriz ZCAWhitening a partir da PCAWhitening e U, atraves
+da formula estabelecida (UxPCAWhite).'''
+
+
+def ZCAWhitening(xPCAWhite, U):
+    xZCAWhite = U.dot(xPCAWhite)  # formula da ZCAWhitening
+    return xZCAWhite
+
+
+'''Funcao responsavel por receber a matriz 2D NxM da imagem e retornar uma nova matriz 1 x N*M, sem alteracao
+dos valores da mesma. Todos os valores foram transformados para float, pois quando lidamos com int temos problema
+de overflow em algumas contas.'''
+
+
+def reshapeImage(img):
+    vector = img.flatten(1)
+    x = vector.reshape(1, len(vector))
+    x = x.astype('float64')
+    return x
+
+
+'''Funcao responsavel por abrir uma janela do sistema com o titulo escolhido para apresentar a imagem do parametro.'''
+
+
+def showImage(img, title):
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
+
+
+'''Funcao responsavel por transformar a matriz ZCAWhite que possui dimensao 1xN*M em uma nova matriz, equivalente a original
+de dimensoes NxM, sem alteracao de seus valores. Apresenta a imagem final caso a opcao seja escolhida.'''
+
+
+def shapeImageWhitened(xZCAWhite, width, height):
+    reshaped = xZCAWhite.reshape(height, width)
+    reshaped_t = reshaped.T
+    # if showImages:
+    # showImage(reshaped_t, 'whitened')
+
+    return reshaped_t
+
 def run():
-    data_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
-    test_files = ['test_batch']
-    train_set, valid_set, test_set = read_images_from_cifar_10(data_files, test_files)
-    img_2 = test_set[0][0]
-    img_2 = unflatten(img_2)
-    draw_img(img_2)
-    img_1 = train_set[100][0]
-    img = unflatten(img_1)
-    draw_img(img)
+    # data_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+    # test_files = ['test_batch']
+    # train_set, valid_set, test_set = read_images_from_cifar_10(data_files, test_files)
+    # img_2 = test_set[0][0]
+    # img_2 = unflatten(img_2)
+    # draw_img(img_2)
+    # img_1 = train_set[100][0]
+    # img = unflatten(img_1)
+    # draw_img(img)
+    img_file = '../girl-original.png'
+    img = cv2.imread(img_file)
+    # img_matplot = plt.imread(img_file)
+    img_whitened = apply_whitening_in_image(img, True)
+    # img_whitened_mp = apply_whitening_in_image(img_matplot)
+    draw_img(img, 'original')
+    draw_img(img_whitened, 'whitened')
     # normalized = normalize_img(img_2)
     # draw_img(normalized)
     # img_nova = flatten_img(normalized)
